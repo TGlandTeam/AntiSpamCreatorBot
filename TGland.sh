758b2beb741f5b2c886e252011b078555dbc2223
SceneTeam <omidbakhsh22@gmail.com>
2016-08-12 22:38:18 +0430

    Update and rename TGland.sh to TGland.sh

--- TGland.sh
+++ TGland.sh
@@ -8,6 +8,7 @@ if [ !$( which screen ) ]; then
   sudo apt-get update
   sudo apt-get upgrade -y
   sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
+  chmod +x launch.sh
   ./launch.sh install
   ./launch.sh
 fi
