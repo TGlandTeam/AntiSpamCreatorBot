5f0e427eab2f9784c363e79da3757eb58c0b2ee5
SceneTeam <omidbakhsh22@gmail.com>
2016-08-12 22:36:53 +0430

    Update and rename TGland.sh to TGland.sh

--- TGland.sh
+++ TGland.sh
@@ -5,7 +5,7 @@
 # Check if user has screen, if not install it
 if [ !$( which screen ) ]; then
   echo "Update Server Ubuntu please wait"
-  sudo apt-update
+  sudo apt-get update
   sudo apt-get upgrade -y
   sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
   ./launch.sh install
