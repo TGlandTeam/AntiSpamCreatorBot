910298b5d3b25bccf84125657f6bf4012b3f365a
SceneTeam <omidbakhsh22@gmail.com>
2016-09-15 08:35:04 -0700

    Update tgland2.sh

--- tgland2.sh
+++ tgland2.sh
@@ -3,6 +3,8 @@
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
+chmod +x launch.sh
+
  sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 
 ./launch.sh install
